3146253df226ec793a8595c736b3e17a2fa6df65
Phil Olson <philip.olson@pm.me>
2021-01-21 03:26:20 +0000

    remove doram

--- src/char/char.c
+++ src/char/char.c
@@ -1732,7 +1732,6 @@ static int char_make_new_char_sql(struct char_session_data *sd, const char *name
 		return flag;
 
 	switch (starting_class) {
-		case JOB_SUMMONER:
 		case JOB_NOVICE:
 			break;
 		default:
